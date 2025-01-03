b4e68b47cf6b4ede900d162e3dcb94f77471d71c
Cezary Bomba <cariusek44@gmail.com>
2025-01-03 14:12:18 +0100

    Main gallery fix

--- webapp/lib/main.dart
+++ webapp/lib/main.dart
@@ -7,77 +7,78 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: "AIzaSyCzcBJXY5tjmq2r_dDwNXXW54-3PhRlEVs",
-      authDomain: "cloudproject-bda5e.firebaseapp.com",
-      projectId: "cloudproject-bda5e",
-      storageBucket: "cloudproject-bda5e.appspot.com",
-      messagingSenderId: "52378970196",
-      appId: "1:52378970196:web:5aea3b07e55812f770e5c1",
-      measurementId: "G-JR3HPNQLQS",
-    ),
+        apiKey: "AIzaSyCzcBJXY5tjmq2r_dDwNXXW54-3PhRlEVs",
+        authDomain: "cloudproject-bda5e.firebaseapp.com",
+        projectId: "cloudproject-bda5e",
+        storageBucket: "cloudproject-bda5e.firebasestorage.app",
+        messagingSenderId: "52378970196",
+        appId: "1:52378970196:web:5aea3b07e55812f770e5c1",
+        measurementId: "G-JR3HPNQLQS"),
   );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      title: 'Galeria Firebase',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
     );
   }
 }
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  void _login() async {
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      );
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => GalleryScreen()),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Login failed: ${e.toString()}")),
-      );
-    }
-  }
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
+      appBar: AppBar(title: Text('Logowanie')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
             ),
             TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: "Password"),
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Hasło'),
               obscureText: true,
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _login,
-              child: Text("Login"),
+              onPressed: () async {
+                try {
+                  UserCredential userCredential =
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+
+                  // Przejście do galerii po zalogowaniu
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GalleryPage(user: userCredential.user)),
+                  );
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Logowanie nie powiodło się: $e')),
+                  );
+                }
+              },
+              child: Text('Zaloguj się'),
             ),
           ],
         ),
@@ -86,71 +87,59 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-class GalleryScreen extends StatefulWidget {
-  @override
-  _GalleryScreenState createState() => _GalleryScreenState();
-}
-
-class _GalleryScreenState extends State<GalleryScreen> {
-  final FirebaseStorage _storage = FirebaseStorage.instance;
-  List<String> _imageUrls = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadImages();
-  }
-
-  void _loadImages() async {
-    try {
-      final ListResult result = await _storage.ref("IMAGES/OG").listAll();
-      final List<String> urls = await Future.wait(
-        result.items.map((item) => item.getDownloadURL()).toList(),
-      );
-      setState(() {
-        _imageUrls = urls;
-      });
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Failed to load images: ${e.toString()}")),
-      );
-    }
-  }
+class GalleryPage extends StatelessWidget {
+  final User? user;
+  GalleryPage({required this.user});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Gallery")),
-      body: _imageUrls.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8.0,
-                mainAxisSpacing: 8.0,
-              ),
-              itemCount: _imageUrls.length,
-              itemBuilder: (context, index) {
-                return Image.network(
-                  _imageUrls[index],
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                            : null,
-                      ),
-                    );
-                  },
-                  errorBuilder: (context, error, stackTrace) {
-                    return Center(child: Text('Error loading image'));
-                  },
-                );
-              },
-            ),
+      appBar: AppBar(
+        title: Text('Galeria'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: FutureBuilder(
+        future: FirebaseStorage.instance.ref('IMAGES/OG').listAll(),
+        builder: (context, AsyncSnapshot<ListResult> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Wystąpił błąd: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.items.isEmpty) {
+            return Center(child: Text('Brak zdjęć w katalogu'));
+          }
+
+          final files = snapshot.data!.items;
+
+          return GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemCount: files.length,
+            itemBuilder: (context, index) {
+              return FutureBuilder(
+                future: files[index].getDownloadURL(),
+                builder: (context, AsyncSnapshot<String> urlSnapshot) {
+                  if (urlSnapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (urlSnapshot.hasError) {
+                    return Center(child: Icon(Icons.error));
+                  } else {
+                    return Image.network(urlSnapshot.data!, fit: BoxFit.cover);
+                  }
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
